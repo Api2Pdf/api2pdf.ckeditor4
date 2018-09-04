c92025902f8dea5ce0bd935adade12d8de65a0b9
Kunal Johar <kjohar@getopenwater.com>
2018-09-04 19:44:32 -0400

    Finish plugin code

--- plugins/save-to-pdf/plugin.js
+++ plugins/save-to-pdf/plugin.js
@@ -3,7 +3,31 @@ CKEDITOR.plugins.add( 'save-to-pdf', {
     init: function( editor ) {
         editor.addCommand( 'save-to-pdf', {
             exec: function( editor ) {
-                debugger;
+                //Do POST without JQUERY
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', editor.config.pdfHandler);
+                xhr.setRequestHeader('Content-Type', 'application/json');      
+                xhr.onload = function() 
+                {
+                    if (xhr.status === 200) {
+                    var result = JSON.parse(xhr.responseText);
+
+                    if(result && result.pdfUrl)
+                        window.location.href = result.pdfUrl;
+                    else
+                        console.log(xhr.resposneText);
+                        
+                    document.body.style.cursor='default';
+                    }
+                    else
+                    {
+                    document.body.style.cursor='default';
+                    console.log("Error generating pdf");
+                    }
+                };
+
+                xhr.send(JSON.stringify({content: editor.getData()}));
+                document.body.style.cursor='wait';
             }
         } );    
         editor.ui.addButton( 'save-to-pdf', {

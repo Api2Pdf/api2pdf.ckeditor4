d70ee4ca390267fc8bebf61c7decede7f1539a0f
Kunal Johar <kjohar@getopenwater.com>
2018-09-04 19:27:39 -0400

    Update to plugin code

--- plugins/save-to-pdf/plugin.js
+++ plugins/save-to-pdf/plugin.js
@@ -1,11 +1,15 @@
-editor.addCommand( 'save-to-pdf', {
-        exec: function( editor ) {
-            debugger;
-        }
-    } );
-
-editor.ui.addButton( 'save-to-pdf', {
-    label: 'Save as PDF',
-    command: 'save-to-pdf',
-    toolbar: 'tools'
+CKEDITOR.plugins.add( 'save-to-pdf', {
+    icons: 'save-to-pdf',
+    init: function( editor ) {
+        editor.addCommand( 'save-to-pdf', {
+            exec: function( editor ) {
+                debugger;
+            }
+        } );    
+        editor.ui.addButton( 'save-to-pdf', {
+            label: 'Save as PDF',
+            command: 'save-to-pdf',
+            toolbar: 'tools'
+        });
+    }
 });
